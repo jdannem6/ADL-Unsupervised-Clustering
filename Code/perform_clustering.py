9ad74d27440e81b5547d90af6b4d27cf7e60609c
Justin Dannemiller <jdannem6@kent.edu>
2023-04-15 19:36:48 -0400

    Added new script to cluster encoded data

---
+++ Code/perform_clustering.py
@@ -0,0 +1,10 @@
+###############################################################################
+### Script name:     perform_clustering.py                                  ###
+### Script function: Takes the encoded dataframe, performs PCA upon it and  ###
+###                  performs unsupervised clustering on resulting          ###
+###                  components                                             ###
+### Authors:         Justin Dannemiller, Keith Machina, and Bailey Wimer    ###
+### Last Modified: 04/14/2023                                               ###
+###############################################################################
+
+from encode_data import get_encoded_df
