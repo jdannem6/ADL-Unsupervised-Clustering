7255e9269334324fb05065876bd191f6e67a83e6
Justin Dannemiller <jdannem6@kent.edu>
2023-04-15 19:36:13 -0400

    Finished encoding script

--- Code/encode_data.py
+++ Code/encode_data.py
@@ -1,5 +1,5 @@
 ###############################################################################
-### Script name:     encode_data.py                                  ###
+### Script name:     encode_data.py                                         ###
 ### Script function: encodes the attributes of preprocessed dataframe such  ###
 ###                  that each value is numerical can be used to train      ###
 ###                  clustering model                                       ###
@@ -13,10 +13,27 @@ from sklearn.preprocessing import OneHotEncoder # Needed for encoding categorica
 import numpy as np # Needed for number processing and array manipulation
 import os # path creation
 
-DEBUG_MODE = False
-WRITE_DF_TO_CSV = True
+DEBUG_MODE = True
+WRITE_DF_TO_CSV = False
 
-if __name__ == "__main__":
+## Converts a datetime.time object from %H%M%S format to seconds
+# Takes string form of time object and returns number of seconds
+def convert_time_to_secs(time_str):
+    # Split the string using ":" as delimiter (assumes time is delimited 
+    # with colons
+    time_components = time_str.split(":")
+    hours = int(time_components[0])
+    minutes = int(time_components[1])
+    seconds = int(time_components[2])
+
+    total_seconds = 3600*hours + 60*minutes + seconds
+    return total_seconds
+
+# Main function of this program, takes the preprocessed dataframe
+# creates an encoded dataframe from them
+# Returns encoded dataframe, the input dataframe containing unencoded data
+# and the classifications dataframe storing true classes of each sample
+def get_encoded_df():
     # If debugging, skip these steps, and read dataframes directly from csv
     # to prevent need to recreate dataframes every run
     if not DEBUG_MODE:
@@ -46,9 +63,9 @@ if __name__ == "__main__":
         # the dataframes every time
         if WRITE_DF_TO_CSV:
             model_input_csv_path = os.getcwd() + "/Processed_Dataframes/model_input.csv"
-            model_input_df.to_csv(model_input_csv_path)
+            model_input_df.to_csv(model_input_csv_path, index=False)
             classifications_csv_path = os.getcwd() + "/Processed_Dataframes/classifications.csv"
-            classifications_df.to_csv(classifications_csv_path)
+            classifications_df.to_csv(classifications_csv_path, index=False)
 
     # If in debug mode, read the dataframes directly from csv
     else:
@@ -57,20 +74,17 @@ if __name__ == "__main__":
         classifications_csv_path = os.getcwd() + "/Processed_Dataframes/classifications.csv"
         classifications_df = pd.read_csv(classifications_csv_path)
 
-    print(model_input_df)
     ### Encode the categorical attributes to obtain numeric representations
     ### that can be used for model training
-    encoded_df = model_input_df.copy(deep=False)
+    encoded_df = pd.DataFrame()
     # Create list of categorical attributes that need to be encoded
-    categorical_attributes = ['Location', 'Type', 'Activity']
+    categorical_attributes = ['Location', 'Type', 'Place', 'Activity', 'Day of Week']
     # For each of the categorical variables, fit an encoder and transform
     # to obtain one hot encoding
     encoder_dict = {}
-    location_encoder = OneHotEncoder()
-    location_reshaped = np.array(model_input_df['Location']).reshape(-1, 1)
-    encoded_values = location_encoder.fit_transform(location_reshaped)
-    print(encoded_values.toarray()[:5])
     for attr in categorical_attributes:
+        # Get the names of all unique classes
+        unique_class_names = model_input_df[attr].unique()
         # Create the encoder
         encoder_dict[attr] = OneHotEncoder()
         # Convert attribute values to two-dimensional matrix for one hot
@@ -81,10 +95,28 @@ if __name__ == "__main__":
         encoder_dict[attr].fit(reshaped_values)
         # Transform encoder to retrieve encoded value
         encoded_attr_values = encoder_dict[attr].fit_transform(reshaped_values)
-        # Replaced the attribute in dataframe with corresponding encoded column
-        # encoded_df.replace(encoded_df.loc[:, attr], encoded_attr_values)
+
+        # Store one hot encoded attribute in dataframe
+        encoded_attr_df = pd.DataFrame(encoded_attr_values.toarray(), 
+                                       columns=unique_class_names)
+        # Add this encoded attribute df to the encoded dataframe
+        encoded_df = pd.concat([encoded_df, encoded_attr_df], axis=1)
+
     ## Since hour is continuous and cyclical, encode it using sine and
     ## cosine function (e.,g since hour 24 is closer to 1 than 22)
+    hours_in_day = 24
+    encoded_df['cos_hour'] = np.cos(2*np.pi*model_input_df['Hour of Day']/hours_in_day)
+    encoded_df['sin_hour'] = np.sin(2*np.pi*model_input_df['Hour of Day']/hours_in_day)
+
+
+    ## Convert Duration attribute as number of seconds
+    encoded_df['Duration'] = ''
+    for i in range(len(model_input_df.index)):
+        time_str = model_input_df.loc[i, 'Duration']
+        time_in_sec = convert_time_to_secs(time_str)
+        encoded_df.at[i, 'Duration'] = time_in_sec
+    
+    return encoded_df, model_input_df, classifications_df
 
 
 
