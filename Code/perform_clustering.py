a6ee0b35262dec2bb4c4b52780afbf91f45a6919
NerosJourney <sandman4264@gmail.com>
2023-04-16 19:15:40 -0400

    Fixed confusion matrix

--- Code/perform_clustering.py
+++ Code/perform_clustering.py
@@ -10,9 +10,11 @@
 from encode_data import get_encoded_df
 from sklearn.decomposition import PCA # Needed for performing PCA on encoded df
 import pandas as pd
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import plotly.express as px
 import os
 if __name__ == "__main__":
     # Load the encoded dataframe
@@ -38,11 +40,10 @@ if __name__ == "__main__":
                           columns=['Feature %s' % i 
                                    for i in range(desired_feature_count)])
     
-    # plot.plot(pca_model.explained_variance_ratio_)
-    # plot.ylabel('Explained Variance')
-    # plot.xlabel('Components')
-    # plot.show()
-
+    # plt.plt(pca_model.explained_variance_ratio_)
+    # plt.ylabel('Explained Variance')
+    # plt.xlabel('Components')
+    # plt.show()
 
 
 pca_result = pca_model.fit_transform(norm_encoded_df)
@@ -71,17 +72,58 @@ cluster_map.to_csv(cluster_map_csv_path, index=False)
 # this is useful for confusion matrix calculation
 index_map = list(set(class_df['True Classification']))
 
-confusion_matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+confusion_matrix = np.zeros((11,11))
 for i in range(len(cluster_map.index)):
     index_of_pred = cluster_map.loc[i, 'cluster']
     actual_class = cluster_map.loc[i, 'True Classification']
     index_of_actual = index_map.index(actual_class)
     confusion_matrix[index_of_actual][index_of_pred] +=1
 print("Confusion matrix: ")
-for i in range(len(confusion_matrix)):
-    print(confusion_matrix[i])
+print(confusion_matrix)
+
+def maximize_diagonal(A):
+    """
+    Given a matrix A, rearranges its rows to maximize the sum of its diagonal elements.
+    Returns the rearranged matrix.
+    """
+    n = A.shape[0]
+    row_indices = np.arange(n)
+    diag_indices = np.arange(n)
+    max_sum = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            new_row_indices = np.copy(row_indices)
+            new_row_indices[i], new_row_indices[j] = new_row_indices[j], new_row_indices[i]
+            new_sum = 0
+            for x in range(n):
+                new_sum += A[new_row_indices[x]][x]
+            if new_sum > max_sum:
+                max_sum = new_sum
+                row_indices = new_row_indices
+    new_arr = np.zeros(A.shape)
+    for n, x in enumerate(row_indices):
+        new_arr[n] = A[x]
+    return new_arr, row_indices
+    
+
+print(maximize_diagonal(confusion_matrix)[0])
+    
+
+
+
+
+tsne = TSNE(n_components=3, random_state=0)
+
+proj_3d = tsne.fit_transform(norm_encoded_df)
+
+fig = px.scatter_3d(
+    proj_3d, x=0, y=1, z=2,
+    color=kmeans_model.labels_
+)
+
+fig.update_traces(marker_size=4)
+fig.show()
+
 
 ## Confusion matrix indicates result is not that good for person A but decent for person B
 # different results each time you run
